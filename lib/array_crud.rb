df016e7460b95e518944fb1cabb9c114c1cd188c
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-02 23:45:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,24 @@
 def create_an_empty_array
-  
+  return []
 end
 
 def create_an_array
-  
+  return [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = []
+  return array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = []
+  return array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = []
+  return array.push(element)
 end
 
 def remove_element_from_start_of_array(array)
