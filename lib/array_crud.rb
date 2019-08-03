2335631712f392893c089989324873a0e3f0a06c
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-03 00:50:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -35,5 +35,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  return array.insert(index_number)
+  array.insert(index_number)
+  return array
 end
