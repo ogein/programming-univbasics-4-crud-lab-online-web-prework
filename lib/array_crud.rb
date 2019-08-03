a25025dd0868d754a4f1090604ac439e22600a82
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-03 00:42:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,41 +7,33 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = []
   return array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = []
   return array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = []
   return array.pop(element)
 end
 
 def remove_element_from_start_of_array(array)
-  array = []
   return array.shift(element)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = []
   return array.at(index_number)  
 end
 
 def retrieve_first_element_from_array(array)
-  array = []
   return array.first
 end
 
 def retrieve_last_element_from_array(array)
-  array = []
   return array.last
 end
 
 def update_element_from_index(array, index_number, element)
-  array = []
   return array.insert(index_number)
 end
